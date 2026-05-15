7e8a100dc79f7f75355f8d22851448b9e2eb0e4a
Elhateknolojia <curtisombai@gmail.com>
2026-05-15 23:11:30 +0300

    Almost , left to wire cover letter and pdf editor with backed

--- main.go
+++ main.go
@@ -119,6 +119,8 @@ func main() {
 corsHandler := ghandlers.CORS(
     ghandlers.AllowedOrigins([]string{
         // "http://localhost:4200",              // Angular dev
+        "http://localhost:4000",
+        "http://localhost:2000",
         "http://localhost:3000",              // if you test on 3000
         "https://resume-six-dun.vercel.app",
         // "*",/ // Allow all origins (for development, be cautious in production)
